433f5a1588e14626bdcf675b3540a8085d1bebec
fyra <fyra@foxmail.com>
2024-07-22 02:32:57 +0800

    remove some stupid notes

--- inc/common.h
+++ inc/common.h
@@ -3,15 +3,7 @@
 #include <vector>
 #include <cstdint>
 
-/**
- * @brief bounding box structure
- * x: x coordinate of centor of the bounding box
- * y: y coordinate of centor of the bounding box
- * w: width of the bounding box
- * h: height of the bounding box
- * score: confidence score of the bounding box
- * cls: class label of the bounding box
- */
+
 typedef struct {
     float x;
     float y;
@@ -21,14 +13,6 @@ typedef struct {
     uint32_t cls;
 }BBox;
 
-
-/**
- * @brief bounding box structure
- * x: x coordinate of the centor of the bounding box
- * y: y coordinate of the centor of the bounding box
- * w: width of the bounding box
- * h: height of the bounding box
- */
 typedef struct {
     uint32_t x;
     uint32_t y;
@@ -37,13 +21,6 @@ typedef struct {
 }xywh_box;
 
 
-/**
- * @brief bounding box structure
- * x1: x coordinate of the top left corner of the bounding box
- * y1: y coordinate of the top left corner of the bounding box
- * x2: x coordinate of the bottom right corner of the bounding box
- * y2: y coordinate of the bottom right corner of the bounding box
- */
 typedef struct {
     uint32_t x1;
     uint32_t y1;

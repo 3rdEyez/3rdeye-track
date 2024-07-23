6da4b2258731762fcfdbe0295af6847a7d1e391c
fyra <fyra@foxmail.com>
2024-07-24 00:41:55 +0800

    add more functions to detect_utils

--- inc/detect_utils.h
+++ inc/detect_utils.h
@@ -2,10 +2,22 @@
 #define __DETECT_UTILS_H__
 
 #include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
 #include <vector>
+#include <queue>
 #include <unordered_map>
 #include <cstdint>
+#include <iomanip>
+#include <sstream>
+#include <string>
 
+
+typedef struct {
+    float x;
+    float y;
+    float w;
+    float h;
+}Box;
 
 typedef struct {
     float x;
@@ -38,6 +50,9 @@ typedef struct {
 } letterbox_info;
 
 
+typedef std::deque<BBox> track_t;
+typedef std::unordered_map<uint64_t, track_t> tracks_t;
+
 const int color_list[80][3] = {
     //{255, 255, 255}, //bg
     {216,  82,  24}, {236, 176,  31}, {125,  46, 141}, {118, 171,  47}, { 76, 189, 237},
@@ -61,7 +76,11 @@ const int color_list[80][3] = {
 
 void letterbox(cv::Mat& img, int new_width, int new_height, letterbox_info& info);
 std::vector<BBox> nms(std::vector<BBox> &bboxes, float iou_threshold);
+std::vector<BBox> bbox_select(const std::vector<BBox> &bboxes, uint32_t cls_to_select);
 BBox_xyxy xywh2xyxy(BBox_xywh box);
+float bbox_iou(BBox_xyxy box1, BBox_xyxy box2);
 BBox xywh2xyxy(float x, float y, float w, float h);
-
+void draw_box_in_color(cv::Mat& img, Box box, cv::Scalar color, int thickness);
+void draw_bboxes(cv::Mat& img, const std::vector<BBox> &bboxes, std::vector<std::string> &classes, float text_scale, int thickness);
+void draw_tracks(cv::Mat& img, tracks_t &tracks, int thickness);
 #endif //__DETECT_UTILS_H__
--- src/detect_utils.cpp
+++ src/detect_utils.cpp
@@ -1,5 +1,11 @@
-#include <opencv2/imgproc.hpp>
 #include "detect_utils.h"
+
+std::string float_to_string(float value, int precision) {
+    std::ostringstream oss;
+    oss << std::fixed << std::setprecision(precision) << value;
+    return oss.str();
+}
+
 
 void letterbox(cv::Mat& img, int new_width, int new_height, letterbox_info& info)
 {
@@ -74,4 +80,78 @@ std::vector<BBox> nms(std::vector<BBox> &bboxes, float iou_threshold)
     }
     
     return result;
+}
+
+
+std::vector<BBox> bbox_select(const std::vector<BBox> &bboxes, uint32_t cls_to_select)
+{
+    std::vector<BBox> result;
+    for (auto item : bboxes) {
+        if (item.cls == cls_to_select) {
+            result.push_back(item);
+        }
+    }
+    return result;
+}
+
+void draw_box_in_color(cv::Mat& img, Box box, cv::Scalar color, int thickness)
+{
+    cv::Point topLeft(static_cast<int>(box.x - box.w / 2), static_cast<int>(box.y - box.h / 2));
+    cv::Point bottomRight(static_cast<int>(box.x + box.w / 2), static_cast<int>(box.y + box.h / 2));
+    cv::rectangle(img, topLeft, bottomRight, color, thickness);
+}
+
+void draw_bboxes(cv::Mat& img,const std::vector<BBox> &bboxes, std::vector<std::string> &classes, float text_scale, int thickness)
+{
+    cv::Scalar color(0, 255, 0); // 边界框颜色，可以根据需要更改
+    cv::Scalar textColor(0, 0, 255); // 文本颜色，白色
+    cv::Point textOrg;
+    int fontFace = cv::FONT_HERSHEY_SIMPLEX; // 选择字体风格
+
+    for (size_t i = 0; i < bboxes.size(); i++) {
+        // 计算边界框的左上角和右下角坐标
+        cv::Point topLeft(static_cast<int>(bboxes[i].x - bboxes[i].w / 2), static_cast<int>(bboxes[i].y - bboxes[i].h / 2));
+        cv::Point bottomRight(static_cast<int>(bboxes[i].x + bboxes[i].w / 2), static_cast<int>(bboxes[i].y + bboxes[i].h / 2));
+        // 绘制边界框
+        cv::rectangle(img, topLeft, bottomRight, color, thickness);
+
+        // 确保类别索引在范围内
+        if (bboxes[i].cls < classes.size()) {
+            // 绘制文本
+            std::string label = classes[bboxes[i].cls] + float_to_string(bboxes[i].score, 2);
+            int baseLine = 0;
+            textOrg = cv::Point(topLeft.x, topLeft.y - 5); // 文本位置在边界框上方
+
+            // 计算文本大小
+            cv::Size labelSize = cv::getTextSize(label, fontFace, text_scale, thickness, &baseLine);
+
+            // 绘制文本背景，确保背景可以完全覆盖文本
+            cv::rectangle(img, cv::Point(textOrg.x, textOrg.y + baseLine - labelSize.height - 5),
+                          cv::Point(textOrg.x + labelSize.width, textOrg.y + baseLine + thickness), color, cv::FILLED);
+
+            // 绘制文本
+            cv::putText(img, label, textOrg, fontFace, text_scale, textColor, thickness);
+        }
+    }
+}
+
+
+void draw_tracks(cv::Mat& img, tracks_t &tracks, int thickness)
+{
+    for (auto &item : tracks) {
+        auto track_id = item.first;
+        auto track = item.second;
+        if (track.size() > 1) {
+            for (size_t i = 1; i < track.size(); i++) {
+                auto p1 = track[i];
+                auto p2 = track[i - 1];
+                int x1 = static_cast<int>(p1.x),
+                    y1 = static_cast<int>(p1.y),
+                    x2 = static_cast<int>(p2.x),
+                    y2 = static_cast<int>(p2.y);
+                    auto c = color_list[track_id % 80];
+                cv::line(img, cv::Point(x1, y1), cv::Point(x2, y2), cv::Scalar(c[0], c[1], c[2]), thickness);
+            }
+        }
+    }
 }

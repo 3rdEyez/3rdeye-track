9cacc137815a275fb5ff6b7afe875bbc4f363965
fyra <fyra@foxmail.com>
2024-07-24 00:40:25 +0800

    add morecases in kalman filter templates

--- test/kalman_filter_test/main.cpp
+++ test/kalman_filter_test/main.cpp
@@ -1,13 +1,19 @@
 #include "kalman_filter.h"
-#include <cstdio>
+#include <iostream>
 
 
 int main(int argc, char const *argv[])
 {
-    Eigen::Matrix<float, 7, 1> x_initial;
-    auto A = Eigen::Matrix<float, 7, 7>::Identity();
-    auto B = Eigen::Matrix<float, 7, 7>::Zero();
-    auto H = Eigen::Matrix<float, 7, 7>::Zero();
-    KalmanFilter<7> kf(x_initial, 0.1f, 1.0f, A, B, H);
+    auto x_initial = Eigen::VectorXf(7);
+    auto A = Eigen::MatrixXf::Identity(7, 7);
+    auto B = Eigen::MatrixXf::Zero(7, 7);
+    auto H = Eigen::MatrixXf::Identity(7, 7);
+    KalmanFilter kf(7, x_initial, 0.1f, 1.0f, A, B, H);
+
+    // print all matrixs
+    std::cout << "x_initial: \n" << x_initial << std::endl;
+    std::cout << "A: \n" << A << std::endl;
+    std::cout << "B: \n" << B << std::endl;
+    std::cout << "H: \n" << H << std::endl;
     return 0;
 }

f47d41534e598e008d2cad4a590ffa9cc3739749
Fyra-BH <fyra@foxmail.com>
2024-07-30 19:40:01 +0800

    修改了yunet demo的传参

--- test/yunet_test/main.cpp
+++ test/yunet_test/main.cpp
@@ -1,41 +1,85 @@
 #include <iostream>
 #include <chrono>
+#include <thread>
+#include <mutex>
+#include <queue>
 #include "yunet_ncnn_detector.h"
 #include "opencv2/highgui.hpp"
 #include "tracker.h"
 
-YunetNCNN detector(
-    "E:/Repo/3rdeye-track/models/ncnn/yunet_n_320_320_ncnn_model/yunet_n_320_320.param",
-    "E:/Repo/3rdeye-track/models/ncnn/yunet_n_320_320_ncnn_model/yunet_n_320_320.bin",
-    true
-);
+std::mutex mtx;
+std::queue<cv::Mat> frame_queue;
 
 int main(int argc, char const *argv[])
 {
-    // cv::Mat frame;
-    // cv::VideoCapture cap(0);
-    // sort_tracker tracker(0.1f, 1.0f, 20, 20);
+    if (argc != 4) {
+        std::cerr << "Usage: " << argv[0] << " <ncnn.param> <ncnn.bin> <video_path>" << std::endl;
+        return -1;
+    }
+    YunetNCNN detector(argv[1], argv[2], true);
+    cv::Mat frame;
+    cv::VideoCapture cap(argv[3]);
+    cap >> frame; // 读取第一帧
+    cv::VideoWriter writer("out.avi", cv::VideoWriter::fourcc('M', 'J', 'P', 'G'), 30, frame.size());
+    int tracker = tracker_init(0.1f, 1.0f, 20, 20);
+    auto start = std::chrono::steady_clock::now();
+    auto end = std::chrono::steady_clock::now();
 
-    // if (!cap.isOpened()) return -1;
-    // while (true)
-    // {    
-    //     cap.read(frame); // 读取一帧
-    //     if(frame.empty()) {
-    //         std::cerr << "Error: Blank frame grabbed" << std::endl;
-    //         break;
-    //     }
-    //     auto bboxes = detector.detect(frame, 0.6f, 0.5f);
-    //     tracker.exec(bboxes);
-    //     auto all_tracks = tracker.get_all_tracks();
-    //     draw_tracks(frame, all_tracks, 1);
-    //     cv::imshow("yunet", frame);
-    //     if (cv::waitKey(1) == 27) break; // 按下ESC键退出
-    // }
-    // cap.release();
-    // cv::destroyAllWindows();
-    // return 0;
+    std::thread writer_thread([&]() {
+        static int cnt = 0;
+        while (1) {
+            mtx.lock();
+            if(frame_queue.empty()) {
+                if (cnt > 100) {
+                    mtx.unlock();
+                    break;
+                }
+                mtx.unlock();
+                std::this_thread::sleep_for(std::chrono::milliseconds(10));
+                cnt += 1;
+                continue;
+            }
+            cv::Mat frame = frame_queue.front();
+            if (frame.empty()) {
+                mtx.unlock();
+                break;
+            }
+            frame_queue.pop();
+            std::cout << " length = " << frame_queue.size() << std::endl;
+            mtx.unlock();
+            writer.write(frame);
+            std::this_thread::sleep_for(std::chrono::milliseconds(20));
+            cnt = 0;
+        }
+    });
 
-    
+    if (!cap.isOpened()) return -1;
+    while (true)
+    {   
+        start = std::chrono::steady_clock::now();
+        cap.read(frame); // 读取一帧
+        if(frame.empty()) {
+            std::cerr << "Error: Blank frame grabbed" << std::endl;
+            break;
+        }
+        auto bboxes = detector.detect(frame, 0.6f, 0.5f);
+        tracker_update(tracker, bboxes);
+        auto all_tracks = tracker_get_tracks(tracker);
+        draw_tracks(frame, all_tracks, 1);
+        mtx.lock();
+        frame_queue.push(frame);
+        mtx.unlock();
+        end = std::chrono::steady_clock::now();
+        printf("fps = %f\n", 1.0 / std::chrono::duration_cast<std::chrono::duration<double>>(end - start).count());
+    }
+    cap.release();
+    // waitting for video_writer thread to finish
+    writer_thread.join();
+    writer.release();
+    return 0;
+}
+
+/*
     char frame_path_fmt[] = "testvideo_frames/%05d.jpg";
     char out_frame_path_fmt[] = "out_frames/%05d.jpg";
     cv::Mat frame;
@@ -60,4 +104,4 @@ int main(int argc, char const *argv[])
     }
 
     return 0;
-}
+*/

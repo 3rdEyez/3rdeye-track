235a929dbdfc2b05aeb3c5b148d1bb207f75254f
Fyra-BH <fyra@foxmail.com>
2024-08-06 00:46:19 +0800

    use thread to get frame from cap

--- demo/rv1103_rknn_inference/main.cpp
+++ demo/rv1103_rknn_inference/main.cpp
@@ -3,8 +3,14 @@
 #include "cv_utils.h"
 #include <iostream>
 #include <chrono>
+#include <thread>
+#include <mutex>
+#include <queue>
 
-constexpr int LOOP_N = 100;
+std::mutex mtx;
+typedef std::pair<int, cv::Mat> payload_t;
+
+constexpr int LOOP_N = 500;
 
 int main(int argc, char const *argv[])
 {
@@ -12,20 +18,55 @@ int main(int argc, char const *argv[])
     cap.open(0);
     cv::Mat img;
     YunetRKNN detector("yunet_n_320_320.rknn");
+    std::deque<payload_t> imgQ;
     cap >> img;
     if (img.empty()) {
         std::cerr << "open camera failed" << std::endl;
         return -1;
     }
-    auto bboxes = detector.detect(img, 0.8, 0.4);
+
+    std::thread producer([&]() {
+        for (int i = 0; i < LOOP_N; i++) {
+            mtx.lock();
+            if (imgQ.size() > 10) {
+                mtx.unlock();
+                continue;
+            }
+            mtx.unlock();
+            cap >> img;
+            mtx.lock();
+            imgQ.push_back(std::make_pair(i, img));
+            mtx.unlock();
+        }
+    });
+
     auto start = std::chrono::high_resolution_clock::now();
-    for (size_t i = 0; i < LOOP_N; i++) {
-        cap >> img;
-        bboxes = detector.detect(img, 0.8, 0.4);
+    while (true) {
+        cv::Mat img;
+        mtx.lock();
+        if (imgQ.empty()) {
+            mtx.unlock();
+            std::this_thread::sleep_for(std::chrono::milliseconds(10));
+            continue;
+        }
+        auto p = imgQ.front();
+        imgQ.pop_front();
+        mtx.unlock();
+        img = p.second;
+        if (img.empty()) {
+            std::cerr << "read frame failed" << std::endl;
+            return -1;
+        }
+        if (p.first == LOOP_N - 1) {
+            std::cout << "loop end" << std::endl;
+            break;
+        }
+        auto bboxes = detector.detect(img, 0.8, 0.5);
         draw_bboxes(img, bboxes, detector.names, 0.8, 1);
     }
     auto end = std::chrono::high_resolution_clock::now();
     std::cout << "average time: " << std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count() / LOOP_N << "ms" << std::endl;
     cv::imwrite("result.jpg", img);
+    producer.join();
     return 0;
 }

9e88fcbc769d7d3c99ecc13a21bdfa85824c8f71
Fyra-BH <fyra@foxmail.com>
2024-08-24 15:32:53 +0800

    use argv to pass parameters

--- demo/ncnn_predict/main.cpp
+++ demo/ncnn_predict/main.cpp
@@ -6,14 +6,17 @@
 
 int main(int argc, char const *argv[])
 {
-    cv::Mat img = cv::imread(TEST_IMAGE_FILE);
+    if (argc != 4) {
+        std::cerr << "Usage: " << argv[0] << " <ncnn.param> <ncnn.bin> <image_path>" << std::endl;
+        return -1;
+    }
+    
+    ncnn_detector detector(argv[1], argv[2], true);
+
+    std::cout << "image file:" << argv[3] << std::endl;
+    cv::Mat img = cv::imread(argv[3], cv::IMREAD_COLOR);
     cv::imshow("origin", img);
 
-    ncnn_detector detector(
-        YOLOV8_NCNN_MODEL".param",
-        YOLOV8_NCNN_MODEL".bin",
-        true
-    );
     auto bboxes = detector.detect(img, 0.2f, 0.4f);
     for (auto &bbox : bboxes) {
         printf("%f %f %f %f %f %d\n", bbox.x, bbox.y, bbox.w, bbox.h, bbox.score, bbox.cls);

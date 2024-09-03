a0c12e0de48b2ecf29e739140576dc794f047ae0
Fyra-BH <fyra@foxmail.com>
2024-09-04 00:15:05 +0800

    使用命令行传参指定rv1103板的ip

--- demo/rv1103_rknn_inference/run.sh
+++ demo/rv1103_rknn_inference/run.sh
@@ -2,6 +2,13 @@
 
 BUILD_DIR="build"
 
+BOARD_IP=$1
+if [ -z "$BOARD_IP" ]; then
+    echo "Usage: $0 <board_ip>"
+    exit 1
+fi
+echo "board ip: $BOARD_IP"
+
 if [ ! -d $BUILD_DIR ]; then
     mkdir $BUILD_DIR
 fi
@@ -11,7 +18,7 @@ cmake -D CMAKE_BUILD_TYPE=Release -D CMAKE_CXX_FLAGS="-D PROFILER_ON" -D CMAKE_I
 make install -j8
 rm install/lib -r
 rm install/inc -r
-sshpass -p luckfox scp -r install root@192.168.1.11:~
-sshpass -p luckfox ssh root@192.168.1.11 "cd /root/install; ./rv1103_rknn_inference_demo"
-sshpass -p luckfox scp root@192.168.1.11:/root/install/result.jpg .
+sshpass -p luckfox scp -r install root@$BOARD_IP:~
+sshpass -p luckfox ssh root@$BOARD_IP "cd /root/install; ./rv1103_rknn_inference_demo"
+sshpass -p luckfox scp root@$BOARD_IP:/root/install/result.jpg .
 
